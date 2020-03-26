ee34218bc45f497abfac43e3e62f4b48e33870c3
lly <853692043@qq.com>
2020-03-27 01:22:39 +0800

    成分

--- pages/pie/index.js
+++ pages/pie/index.js
@@ -32,6 +32,7 @@ Page({
     type: 0
   },
   onLoad: function (options) {
+    this.echartsComponnet = this.selectComponent('#mychart');
     this.getData();
   },
   getData(){
@@ -42,43 +43,64 @@ Page({
       method: "POST"
     })
     .then(res => {
-      if(!res.data){
+      if(res.data < 1){
         that.setData({
-          show: true
+          show: true,
         })
       }else{
-        chart.setOption({
-          tooltip: {
-            trigger: 'item',
-            formatter: '{b}: {c} ({d}%)'
-          },
-          legend: {
-            orient: 'vertical',
-            left: 10,
-            data: ['蛋白质', '脂肪', '碳水化合物', '卡路里', '无机盐','钙','磷','铁']
-          },
-          backgroundColor: "#ffffff",
-          color: ['#c23531','#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83',  '#ca8622', '#bda29a','#6e7074', '#546570', '#c4ccd3'],
-          series: [{
-            label: {
-              normal: {
-                fontSize: 14
-              }
+        that.setData({
+          show: false,
+        })
+        chart.clear();
+        setTimeout(function(){
+          chart.setOption({
+            tooltip: {
+              trigger: 'item',
+              formatter: '{b}: {c} ({d}%)'
             },
-            type: 'pie',
-            radius: '45%',
-            data: res.data
-          }]
-        });
+            legend: {
+              orient: 'vertical',
+              left: 10,
+              data: ['蛋白质', '脂肪', '碳水化合物', '卡路里', '无机盐','钙','磷','铁']
+            },
+            backgroundColor: "#ffffff",
+            color: ['#c23531','#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83',  '#ca8622', '#bda29a','#6e7074', '#546570', '#c4ccd3'],
+            series: [{
+              label: {
+                normal: {
+                  fontSize: 14
+                }
+              },
+              type: 'pie',
+              radius: '45%',
+              data: res.data
+            }]
+          });
+        },200);
+        
       }
     })
   },
   changeGroup: function(event){
+    var that =this;
     var type = event.currentTarget.dataset.id;
     this.setData({
       type: type
     });
+    
     this.getData();
+  },
+  //初始化图表
+  init_echarts: function () {
+    this.echartsComponnet.init((canvas, width, height) => {
+      chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr // new
+      });
+      canvas.setChart(chart);
+      return chart;
+    });
   }
 });
 
